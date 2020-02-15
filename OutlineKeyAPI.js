395a89fc6a065db3a9ab318b232cf016df3e825e
ffi <azat@rahmanow.com>
2020-02-15 02:58:59 -0800

    Revert "- Added OutlineKeysAPI"
    This reverts commit 25edcf1ecbdfcfed407bc63b165dd455198b3929.

--- OutlineKeyAPI.js
+++ OutlineKeyAPI.js
@@ -5,22 +5,16 @@ const agent = new https.Agent({
 });
 
 // Information to reach API
-const managementApiUrl = ' '; // Copy and paste Management API URL from Outline Manager Key Settings
-const domain = ' '; // set your custom domain if you have one
-
-const url = managementApiUrl + '/access-keys/';
-const ip = managementApiUrl.split('/')[2].split(':')[0];
+const url = 'https://79.143.31.246:61629/x7Dt9XsaNHsUMxIubXPjqg/access-keys/';
+const ip = '79.143.31.246' + ':443/?outline=1';
+const domain = 'zapchas.tk' + ':443';
 
 const getKeys = async () => {
     const response = await fetch(url, {agent});
     const myJson = await response.json();
     const keys = myJson.accessKeys;
     for (let i=0; i<keys.length; i++) {
-        let key = keys[i];
-        //let port = ':' + key['port'];
-        let name = key['name'];
-        let accessUrl = key['accessUrl'];
-        console.log(name + ' - ' + accessUrl.replace(ip, domain));
+        console.log(keys[i]['name'] + ' - ' + keys[i]['accessUrl'].replace(ip, domain));
     }
 };
 getKeys();
